e9cbe916e98e8b1af78356dc8eba7afbab723d34
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-29 00:26:43 -0300

    add routes to address

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const AddressController = require('./controllers/AddressController')
 const UserController = require('./controllers/UserController')
 
 const routes = express.Router()
 
 routes.post('/users', UserController.store)
 routes.get('/users', UserController.list)
+
+routes.post('/users/:user_id/addresses', AddressController.store)
+routes.get('/users/:user_id/addresses', AddressController.list)
 
 module.exports = routes
