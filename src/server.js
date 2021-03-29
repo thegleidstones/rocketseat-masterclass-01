c7bc24ebf42229d545ba440f6b90ebfd7e56c21d
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-28 23:22:43 -0300

    add users routes and import connection to server

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
-const connection = require('../database/index')
+require('./database')
 
 const app = express()
 
@@ -10,10 +10,4 @@ app.use(routes)
 app.listen(3333, async () => {
     console.log("servidor rodando na porta: 3333")
 
-    try {
-        await connection.authenticate();
-        console.log('Postgres Database Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
 })
