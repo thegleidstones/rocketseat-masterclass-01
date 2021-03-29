c17c296d6480e95dea38eaf71caf0c2bf5564ed6
Gleidson Morais Silva <silvamorais.ti@gmail.com>
2021-03-28 21:43:10 -0300

    create database connection

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const routes = require('./routes')
+const connection = require('../database/index')
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => {
+app.listen(3333, async () => {
     console.log("servidor rodando na porta: 3333")
+
+    try {
+        await connection.authenticate();
+        console.log('Postgres Database Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
 })
